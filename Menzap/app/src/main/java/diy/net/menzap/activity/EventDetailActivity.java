5e98c40b130f0d7689dbb3b4012cf09b4bb4f816
Aditya <desh.adi05@gmail.com>
2016-09-20 02:37:58 +0200

    Implementation of Events list with drill down activity

---
+++ Menzap/app/src/main/java/diy/net/menzap/activity/EventDetailActivity.java
@@ -0,0 +1,84 @@
+package diy.net.menzap.activity;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.content.Intent;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import diy.net.menzap.R;
+import diy.net.menzap.fragments.EventsFragment;
+
+public class EventDetailActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_detail);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+        String event = getIntent().getExtras().getString("eventName");
+
+        TextView eventName = (TextView) findViewById(R.id.eventName);
+        eventName.setText(event);
+        TextView startTime = (TextView) findViewById(R.id.startTime);
+        startTime.setText("Event start time : 12:00 PM");
+        TextView endTime = (TextView) findViewById(R.id.endTime);
+        endTime.setText("Event ends at : 02:00 PM");
+        TextView location = (TextView) findViewById(R.id.location);
+        location.setText("Location : First Floor Mensa Garching");
+
+    }
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("EventDetail Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
+
--- Menzap/app/src/main/java/diy/net/menzap/fragments/EventsFragment.java
+++ Menzap/app/src/main/java/diy/net/menzap/fragments/EventsFragment.java
@@ -1,16 +1,22 @@
 package diy.net.menzap.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import diy.net.menzap.R;
+import diy.net.menzap.activity.EventDetailActivity;
 
 
-public class EventsFragment extends Fragment{
-
+public class EventsFragment extends ListFragment implements AdapterView.OnItemClickListener {
+    String[] events =  new String[] { "Hackathon Registration on 1st Floor", "Free Deserts near Ausgabe 2", "Mexican Food Festival starts tomorow" };
     public EventsFragment() {
         // Required empty public constructor
     }
@@ -24,7 +30,30 @@ public class EventsFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events, container, false);
+        View view = inflater.inflate(R.layout.fragment_events, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, events);
+
+        setListAdapter(adapter);
+        getListView().setOnItemClickListener(this);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getActivity(), EventDetailActivity.class);
+        intent.putExtra("eventName", events[position]);
+        intent.putExtra("startTime", "12:00 PM");
+        intent.putExtra("endTime", "02:00 PM");
+        intent.putExtra("location", "First Floor Mensa Garching");
+        startActivity(intent);
     }
 
 }
